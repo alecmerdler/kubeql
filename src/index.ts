9522138f7ae94853ef5cf0df74d3adf2f2d6af1c
alecmerdler <alecmerdler@gmail.com>
2018-05-01 22:41:29 -0400

    working on loading k8s credentials using ServiceAccount/kubeconfig.yaml

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import { buildSchema, graphql } from 'graphql';
+import { promisify } from 'util';
+
+const readFile = promisify(fs.readFile);
+
+export const credentialsFromKubeConfig = async() => {
+    // TODO: Load/parse kubeconfig.yaml
+};
+
+export const credentialsFromCluster = async() => {
+    const rootDir = '/var/run/secrets/kubernetes.io/serviceaccount/';
+    const namespace = await readFile(path.join(rootDir, 'namespace'));
+    const token = await readFile(path.join(rootDir, 'token'));
+    const crt = await readFile(path.join(rootDir, 'ca.crt'));
+
+    return {namespace, token, crt};
+};
 
 const schema = buildSchema(`
     type Query {
@@ -9,3 +27,10 @@ const schema = buildSchema(`
 graphql(schema, '{ hello }').then(response => {
     console.log(response);
 });
+
+export type Credentials = {
+    namespace: string;
+    token: string;
+    crt: string;
+};
+export type CredentialsFrom = () => Promise<{creds: Credentials}>;
